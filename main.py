d19dc6541e247e8780c1b6d96be733a6485ae19e
amzyD <lukasz.dyzma1999@gmail.com>
2025-07-09 19:48:10 +0200

    add groups to Player and call it indirectly in main

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from player import Player
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = [updatable, drawable]
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+
 
     while True:
         for event in pygame.event.get():
@@ -16,12 +22,13 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+        for drawing in drawable:
+            drawing.draw()
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(144) / 1000
-        player.update(dt)
+        updatables.update(dt)
 
 
 if __name__ == "__main__":
