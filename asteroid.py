369be22d97e5d39f06de5e33dee550296ec85414
amzyD <lukasz.dyzma1999@gmail.com>
2025-07-11 20:01:23 +0200

    bug fix in importing from other files

--- asteroid.py
+++ asteroid.py
@@ -1,3 +1,7 @@
+from circleshape import CircleShape
+import pygame
+from constants import *
+
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
